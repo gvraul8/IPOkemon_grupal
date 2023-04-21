d9b2aa78e893ea8c6ca3027d85dcfa5a30dbaec3
Raúl González Velázquez <raul.gonzalez15@alu.uclm.es>
2023-04-21 10:16:53 +0200

    Creacion proyecto

---
+++ PokemonGrupal/MainPage.xaml.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace PokemonGrupal
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += opcionVolver;
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(320, 320));
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBoundsChanged
+           += MainPage_VisibleBoundsChanged;
+
+
+            TileContent content = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+                    TileMedium = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = "IPOkemon",
+                                    HintStyle = AdaptiveTextStyle.Subtitle
+                                },
+                                new AdaptiveText()
+                                {
+                                    Text = "Un proyecto de IPO2",
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+                            }
+                        }
+                    },
+                    TileWide = new TileBinding()
+                    {
+                        Branding = TileBranding.NameAndLogo,
+                        DisplayName = "Version 1.0",
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children = {
+                                new AdaptiveText()
+                                {
+                                    Text = "IPOkemon",
+                                    HintStyle = AdaptiveTextStyle.Subtitle
+                                },
+                                new AdaptiveText()
+                                {
+                                    Text = "Un Proyecto de IPO2",
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+                                new AdaptiveText()
+                                {
+                                    Text = "Una aplicación sobre Pokemon hecha con tecnología UWP",
+                                    HintWrap = true,
+                                }
+                            }
+                        }
+                    },
+                    TileLarge = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children = {
+                                new AdaptiveText()
+                                {
+                                    Text = "IPOkemon",
+                                    HintStyle = AdaptiveTextStyle.Subtitle
+                                },
+                                new AdaptiveText()
+                                {
+                                    Text = "Un Proyecto de IPO2",
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+                                new AdaptiveText()
+                                {
+                                     Text = "Una aplicación sobre Pokemon hecha con tecnología UWP",
+                                     HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                }
+                            }
+                        }
+                    },
+                }
+            };
+
+            var notification = new TileNotification(content.GetXml());
+            notification.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(30);
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.Update(notification);
+
+
+        }
+
+
+        private void NotificaciónSubida(object sender, PointerRoutedEventArgs e)
+        {
+            new ToastContentBuilder()
+            .AddArgument("action", "Favoritos")
+            .AddArgument("conversationId", 9813)
+            .AddText("Golbat ha evolucionado")
+            .AddText("Puedes ver más información en IPOkemon")
+            .Show();
+        }
+
+
+
+        //////////////////////////////////////         Dimensionado de la pantalla    ///////////////////////////////////////////////////////
+        private void MainPage_VisibleBoundsChanged(Windows.UI.ViewManagement.ApplicationView sender, object args)
+        {
+            var Width =
+           Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
+            if (Width >= 720)
+            {
+                spMenu.DisplayMode = SplitViewDisplayMode.CompactInline;
+                spMenu.IsPaneOpen = true;
+            }
+            else if (Width >= 360)
+            {
+                spMenu.DisplayMode = SplitViewDisplayMode.CompactOverlay;
+                spMenu.IsPaneOpen = false;
+            }
+            else
+            {
+                spMenu.DisplayMode = SplitViewDisplayMode.Overlay;
+                spMenu.IsPaneOpen = false;
+            }
+        }
+
+        //////////////////////////////////////         Navegacion entre paginas    ///////////////////////////////////////////////////////
+
+        private void irPokeDex(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(PokeDexPage));
+        }
+        private void irCombate(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(CombatePage));
+        }
+        private void irInicio(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(MainPage));
+        }
+        private void opcionVolver(object sender, BackRequestedEventArgs e)
+        {
+            if (fmMain.BackStack.Any())
+            {
+                fmMain.GoBack();
+            }
+        }
+
+        private void btMenu_Click(object sender, RoutedEventArgs e)
+        {
+            spMenu.IsPaneOpen = !spMenu.IsPaneOpen;
+        }
+    }
+}
---
+++ PokemonGrupal/wartortle.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PokemonGrupal
+{
+    public sealed partial class wartortle : UserControl
+    {
+
+        DispatcherTimer dtTime;
+        DispatcherTimer dtTime2;
+
+        public wartortle()
+        {
+            this.InitializeComponent();
+        }
+
+        ///////////////////////////////////// VIDA ///////////////////////////////////////////////
+
+        private void aumentarVida(object sender, PointerRoutedEventArgs e)
+        {
+            dtTime = new DispatcherTimer();
+            dtTime.Interval = TimeSpan.FromMilliseconds(50);
+            dtTime.Tick += increaseHealth;
+            dtTime.Start();
+            this.imgPocionVida.Opacity = 0.5;
+        }
+
+        private void increaseHealth(object sender, object e)
+        {
+            this.pbVida.Value += 0.2;
+            if (pbVida.Value >= 100)
+            {
+                this.dtTime.Stop();
+                this.imgPocionVida.Opacity = 1;
+            }
+        }
+
+        private void imgPocionVida_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Storyboard vida = (Storyboard)this.Resources["Vida"];
+            vida.Begin();
+
+            this.aumentarVida(sender, e);
+        }
+
+
+        ///////////////////////////////////// ENERGIA ///////////////////////////////////////////////
+
+        private void aumentarEnergia(object sender, PointerRoutedEventArgs e)
+        {
+            dtTime2 = new DispatcherTimer();
+            dtTime2.Interval = TimeSpan.FromMilliseconds(50);
+            dtTime2.Tick += increaseEnergy;
+            dtTime2.Start();
+            this.imgPocionEnergia.Opacity = 0.5;
+        }
+
+        private void increaseEnergy(object sender, object e)
+        {
+            this.pbEnergia.Value += 0.3;
+            if (pbEnergia.Value >= 100)
+            {
+                this.dtTime2.Stop();
+                this.imgPocionEnergia.Opacity = 1;
+            }
+        }
+
+        private void imgPocionEnergia_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Storyboard energia = (Storyboard)this.Resources["Energia"];
+            energia.Begin();
+
+            this.aumentarEnergia(sender, e);
+
+            this.btAtaque1.IsEnabled = true;
+            this.btAtaque2.IsEnabled = true;
+        }
+
+        ///////////////////////////////////// ATAQUES ///////////////////////////////////////////////
+
+        private void btAtaque1_click(object sender, RoutedEventArgs e)
+        {
+            Storyboard ataque1 = (Storyboard)this.Resources["Ataque1"];
+            ataque1.Begin();
+
+            this.pbEnergia.Value -= 20;
+
+            if (pbEnergia.Value <= 0)
+            {
+                // PREGUNTAR COMO PONER TOOLTIP CUANDO ESTAN DESACTIVADOS LOS BOTONES
+                this.btAtaque1.IsEnabled = false;
+                this.btAtaque2.IsEnabled = false;
+            }
+        }
+
+        private void btAtaque2_click(object sender, RoutedEventArgs e)
+        {
+            Storyboard ataque2 = (Storyboard)this.Resources["Ataque2"];
+            ataque2.Begin();
+
+            this.pbEnergia.Value -= 20;
+
+            if (pbEnergia.Value <= 0)
+            {
+                this.btAtaque1.IsEnabled = false;
+                this.btAtaque2.IsEnabled = false;
+            }
+
+        }
+
+        private void btCubrirse_click(object sender, RoutedEventArgs e)
+        {
+            Storyboard cubrirse = (Storyboard)this.Resources["Cubrirse"];
+            cubrirse.Begin();
+        }
+
+
+        ////////////////////////// DORMIR //////////////////////////////////////////
+
+        private void descansar(object sender, RoutedEventArgs e)
+        {
+            dtTime = new DispatcherTimer();
+            dtTime.Interval = TimeSpan.FromMilliseconds(25);
+            dtTime.Tick += sleep;
+            dtTime.Start();
+        }
+
+        private void sleep(object sender, object e)
+        {
+            this.pbVida.Value += 0.2;
+            this.pbEnergia.Value += 0.2;
+            if (pbVida.Value >= 100 && pbEnergia.Value >=100)
+            {
+                this.dtTime.Stop();
+            }
+        }
+
+        private void btDormir_click(object sender, RoutedEventArgs e)
+        {
+            Storyboard dormir = (Storyboard)this.Resources["Dormir"];
+            dormir.Begin();
+
+            this.descansar(sender, e);
+        }
+
+
+        ////////////////////////////////////         GETTERS SETTERS      ///////////////////////////////////////////////////
+
+        public double vida
+        {
+            get { return this.pbVida.Value; }
+            set { this.pbVida.Value = value; }
+        }
+
+    }
+}
